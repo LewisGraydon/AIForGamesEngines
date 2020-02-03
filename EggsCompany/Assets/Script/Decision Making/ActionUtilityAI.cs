c6391819bca6a094fd330f5c6bdf4922ed200683
Chris-Dodds-s6086643 <36199316+Chris-Dodds-s6086643@users.noreply.github.com>
2020-02-03 10:31:02 +0000

    Research & some more planning
    Research read and added to .ppt, classes for Utility based decision making added and some comments relating to the intended implementation added.

---
+++ EggsCompany/Assets/Script/Decision Making/ActionUtilityAI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionUtilityAI : MonoBehaviour
+{
+    //Action: Move, Shoot, Defend, Overwatch
+
+    // move and weight movement heavily for pip one,
+
+    //evaluate shooting based on % to hit, should only shoot first if already in best spot or 100% chance really.
+
+    // defense should be weighted fairly low
+
+    // overwatch similar to defense, maybe should be weighted based on cover of enemy between this and defense.
+}
---
+++ EggsCompany/Assets/Script/Decision Making/MovementUtilityAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementUtilityAI : MonoBehaviour
+{
+
+    // needs to take into account, self cover, if flanking enemy, pip cost, shouldBeShootingPosition 
+
+    // so... Clamp(0, 1, AverageOf((1 - pipCost) + (isFlanking) + (isInCover) + (isShootingPosition)))
+    public void ConsiderTiles(Tile[] tiles)
+    {
+        int bestTileIndex = 0;
+        float bestTileFloat = 0.0f;
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            float tileConsidered = Consider(tiles[0]);
+            if (tileConsidered >= bestTileFloat)
+            {
+                bestTileIndex = i;
+                bestTileFloat = tileConsidered;
+            }
+        }
+    }
+
+    public float Consider(Tile tile)
+    {
+        return 0.0f;
+    }
+}
