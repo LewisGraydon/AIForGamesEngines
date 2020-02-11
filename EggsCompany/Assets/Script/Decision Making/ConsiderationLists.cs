3b91f9e0558ecb7be5a8fb99950475c34493f57e
Chris-Dodds-s6086643 <36199316+Chris-Dodds-s6086643@users.noreply.github.com>
2020-02-11 11:36:57 +0000

    Finished 'Coding' Action Considerations
    Finished the coding (with empty functions in tiles etc.) of the action considerations.

--- EggsCompany/Assets/Script/Decision Making/ConsiderationLists.cs
+++ EggsCompany/Assets/Script/Decision Making/ConsiderationLists.cs
@@ -26,6 +26,15 @@ public static class ConsiderationLists
 //To add a consideration to the enemy agent's A.I :
 // 1. create {verb of Action}Consideration class as a child of the Consideration class
 // 2. add to the relevant considerationList within the above ActionConsiderations class as the others have been.
+
+public enum Weighting
+{
+    Low = 100,
+    Medium = 300,
+    High = 600,
+    guarantee = 10000
+}
+
 public abstract class Consideration
 {
     public Consideration(){}
@@ -68,7 +77,7 @@ public class MoveConsideration : Consideration
         tileValue += self.enemiesInSight.Count < 1 ? 1.0f : 0.0f;
 
         //if the enemy is < 1/2 range
-        return tileValue / numberOfChecksWithinConsideration;
+        return tileValue;
     }
 }
 
@@ -78,11 +87,54 @@ public class OverwatchConsideration : Consideration
     {
         base.ConsiderTile(self, tileToConsider);
         // if distance to enemy is around 1/2 sight range then for this action
+        if (Vector3.Distance(self.transform.position, tileToConsider.transform.position) < (self.sightDistance / 2))
+        {
+            tileValue += (int)Weighting.Medium;
+        }
+        
+        float AgentInCoverTotalValue = 0.0f;
+        float enemiesInCoverTotalValue = 0.0f;
+        foreach (CharacterBase enemy in self.enemiesInSight)
+        {
+            //TODO: calculate direction from enemy to self.
+            // if the agent is in good cover from the enemies then + for this action
+            switch (tileToConsider.ProvidesCoverInDirection(Vector3.forward)) //switch on amount of cover self is in from enemies;
+            {
+                case coverValue.None:
+                    AgentInCoverTotalValue -= (int)Weighting.High / self.enemiesInSight.Count;
+                    break;
+                case coverValue.Half:
+                    AgentInCoverTotalValue += (int)Weighting.Medium / self.enemiesInSight.Count;
+                    break;
+                case coverValue.Full:
+                    AgentInCoverTotalValue += (int)Weighting.Low / self.enemiesInSight.Count;
+                    break;
+                default:
+                    Debug.LogError("issue with cover Value returned");
+                    break;
+            }
 
-        // if the agent is in good cover from the enemies then + for this action
+            // if the enemies are not in full cover then + for this action as expect them to move
+            switch (enemy.occupiedTile.ProvidesCoverInDirection(Vector3.back))
+            {
+                case coverValue.None:
+                    enemiesInCoverTotalValue += (int)Weighting.High / self.enemiesInSight.Count;
+                    break;
+                case coverValue.Half:
+                    enemiesInCoverTotalValue += (int)Weighting.Medium / self.enemiesInSight.Count;
+                    break;
+                case coverValue.Full:
+                    enemiesInCoverTotalValue -= (int)Weighting.Low / self.enemiesInSight.Count;
+                    break;
+                default:
+                    break;
+            }
+        }
+        tileValue += AgentInCoverTotalValue;
+        tileValue += enemiesInCoverTotalValue;
 
-        // if the enemies are not in full cover then + for this action as expect them to move
-        return 0.0f;
+
+        return tileValue;
     }
 }
 
@@ -92,11 +144,55 @@ public class DefendConsideration : Consideration
     {
         base.ConsiderTile(self, tileToConsider);
         // if distance is > 3/4 sight range then + 
+        if(Vector3.Distance(self.transform.position, tileToConsider.transform.position) > (0.75 * self.sightDistance))
+        {
+            tileValue += (int)Weighting.Medium;
+        }
 
         // if agent's level of cover is full then +
-
         // if the enemys cover is full then +
-        return 0.0f;
+        float AgentInCoverTotalValue = 0.0f;
+        float enemiesInCoverTotalValue = 0.0f;
+        foreach (CharacterBase enemy in self.enemiesInSight)
+        {
+            //TODO: calculate direction from enemy to self.
+            // if the agent is in good cover from the enemies then + for this action
+            switch (tileToConsider.ProvidesCoverInDirection(Vector3.forward)) //switch on amount of cover self is in from enemies;
+            {
+                case coverValue.None:
+                    AgentInCoverTotalValue -= (int)Weighting.High / self.enemiesInSight.Count;
+                    break;
+                case coverValue.Half:
+                    AgentInCoverTotalValue -= (int)Weighting.Medium / self.enemiesInSight.Count;
+                    break;
+                case coverValue.Full:
+                    AgentInCoverTotalValue += (int)Weighting.Low / self.enemiesInSight.Count;
+                    break;
+                default:
+                    Debug.LogError("issue with cover Value returned");
+                    break;
+            }
+
+            // if the enemies are not in full cover then + for this action as expect them to move
+            switch (enemy.occupiedTile.ProvidesCoverInDirection(Vector3.back))
+            {
+                case coverValue.None:
+                    enemiesInCoverTotalValue += (int)Weighting.High / self.enemiesInSight.Count;
+                    break;
+                case coverValue.Half:
+                    enemiesInCoverTotalValue += (int)Weighting.Medium / self.enemiesInSight.Count;
+                    break;
+                case coverValue.Full:
+                    enemiesInCoverTotalValue -= (int)Weighting.Low / self.enemiesInSight.Count;
+                    break;
+                default:
+                    break;
+            }
+        }
+        tileValue += AgentInCoverTotalValue;
+        tileValue += enemiesInCoverTotalValue;
+
+        return tileValue;
     }
 }
 
@@ -115,7 +211,56 @@ public class ShootConsideration : Consideration
          * 
          * 
          */
-        return 0.0f;
+        if (self.occupiedTile.chanceToHit(tileToConsider) >= 60)
+        {
+            tileValue += (int)Weighting.High;
+        }
+        if (self.remainingPips == 1)
+        {
+            tileValue += (int)Weighting.Medium;
+        }
+        float AgentInCoverTotalValue = 0.0f;
+        float enemiesInCoverTotalValue = 0.0f;
+        foreach (CharacterBase enemy in self.enemiesInSight)
+        {
+            //TODO: calculate direction from enemy to self.
+            // if the agent is in good cover from the enemies then + for this action
+            switch (tileToConsider.ProvidesCoverInDirection(Vector3.forward)) //switch on amount of cover self is in from enemies;
+            {
+                case coverValue.None:
+                    AgentInCoverTotalValue -= (int)Weighting.High / self.enemiesInSight.Count;
+                    break;
+                case coverValue.Half:
+                    AgentInCoverTotalValue += (int)Weighting.Medium / self.enemiesInSight.Count;
+                    break;
+                case coverValue.Full:
+                    AgentInCoverTotalValue += (int)Weighting.Low / self.enemiesInSight.Count;
+                    break;
+                default:
+                    Debug.LogError("issue with cover Value returned");
+                    break;
+            }
+
+            // if the enemies are not in full cover then + for this action as expect them to move
+            switch (enemy.occupiedTile.ProvidesCoverInDirection(Vector3.back))
+            {
+                case coverValue.None:
+                    enemiesInCoverTotalValue += (int)Weighting.High / self.enemiesInSight.Count;
+                    break;
+                case coverValue.Half:
+                    enemiesInCoverTotalValue += (int)Weighting.Medium / self.enemiesInSight.Count;
+                    break;
+                case coverValue.Full:
+                    enemiesInCoverTotalValue -= (int)Weighting.Low / self.enemiesInSight.Count;
+                    break;
+                default:
+                    break;
+            }
+        }
+        tileValue += AgentInCoverTotalValue;
+        tileValue += enemiesInCoverTotalValue;
+
+        return tileValue;
     }
 }
 
@@ -129,7 +274,22 @@ public class ReloadConsideration : Consideration
         // if numshots == 0 && pips remaining == 1 then guarantee
         //
         // tileValue = 1 - (numshots / maxShots)
-        return 0.0f;
+        if (self.remainingPips == 1)
+        {
+            if (self.remainingShots > (self.maxShots / 2))
+            {
+                tileValue = 0;
+            }
+            else if (self.remainingShots > (self.maxShots / 4) && self.remainingShots != 0)
+            {
+                tileValue += (int)Weighting.High;
+            }
+            else
+            {
+                tileValue += (int)Weighting.guarantee;
+            }
+        }
+        return tileValue;
     }
 }
 #endregion
@@ -150,6 +310,7 @@ public class HitChanceDifferenceConsideration : Consideration
          * 
          * if estimatedHitChance > hitChanceFrom(self.currentTile) then + else - 
         */
+        
         return 0.0f;
     }
 }
