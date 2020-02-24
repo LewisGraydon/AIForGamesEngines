099bfc058f380e2db4b8822eb2d9a39fb87c7a9c
Chris-Dodds-s6086643 <36199316+Chris-Dodds-s6086643@users.noreply.github.com>
2020-02-24 14:40:26 +0000

    Added test functionality to test what cover should be taken into account

---
+++ EggsCompany/Assets/VectorChecker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorChecker : MonoBehaviour
+{
+
+    public GameObject otherFuckingCube;
+    public float coverThreshhold = 0.44f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 normalDirection = (otherFuckingCube.transform.position - this.transform.position).normalized;
+        
+        if(normalDirection.x >= coverThreshhold)
+        {
+            print("observing cover from positive x");
+        }
+        else if (normalDirection.x <= -coverThreshhold)
+        {
+            print("observing cover from negative x");
+        }
+        else
+        {
+            print("normalDirection.x = " + normalDirection.x);
+        }
+        if (normalDirection.z >= coverThreshhold)
+        {
+            print("observing cover from positive z");
+        }
+        else if (normalDirection.z <= -coverThreshhold)
+        {
+            print("observing cover from negative z");
+        }
+        else
+        {
+            print("normalDirection.z = " + normalDirection.z);
+        }
+    }
+}
