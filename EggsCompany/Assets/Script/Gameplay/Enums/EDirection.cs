b8d2d27ca087a6786060f3f1d192d28f7c022854
btbertram <s6108482@live.tees.ac.uk>
2020-02-25 18:09:04 +0000

    Updated EDirection
    -Added four new directions in order to facilitate diagonal movement
    -Added some commentary noting how the cardinal directions relate to 3D Coordinate systems in game: Positive X Axis == East and Positive Z Axis == North

--- EggsCompany/Assets/Script/Gameplay/Enums/EDirection.cs
+++ EggsCompany/Assets/Script/Gameplay/Enums/EDirection.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Positive X Axis == East
+//Positive Z Axis == North
 public enum EDirection
 {
     North,
+    NorthEast,
     East,
+    SouthEast,
     South,
+    SouthWest,
     West,
+    NorthWest,
     Error
 }
